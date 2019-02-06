1816972e8fabf6c549412c6b4dc7b9e8d2242a64
NoussaOns <38370333+NoussaOns@users.noreply.github.com>
2019-02-06 12:11:58 +0400

    update to only one fragment layout

--- app/src/main/java/com/example/android/tourguideapp/MainActivity.java
+++ app/src/main/java/com/example/android/tourguideapp/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.android.tourguideapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +22,25 @@ public class MainActivity extends AppCompatActivity {
         //Find the view pager
         ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
 
-        // create an adapter
-        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+        //pager adapter
+        PagerAdapter adapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            int[] photos = {R.drawable.vacation_holidays_background_with_realistic_globe_suitcase_photo_camera_1284_10476, R.drawable.world_with_landmarks_background_watercolor_style_23_2147765646, R.drawable.modern_hotel_reception_composition_23_2147994128};
+
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                Fragment fragment = new PicturesFragment();
+                Bundle args = new Bundle();
+                args.putInt("photo", photos[position]);
+                fragment.setArguments(args);
+                return fragment;
+            }
+
+            @Override
+            public int getCount() {
+                return photos.length;
+            }
+        };
 
         //set the adapter into the viewPager
         viewPager.setAdapter(adapter);
---
+++ app/src/main/java/com/example/android/tourguideapp/PicturesFragment.java
@@ -0,0 +1,42 @@
+package com.example.android.tourguideapp;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PicturesFragment extends Fragment {
+
+    private int photo;
+
+    public PicturesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        photo = args.getInt("photo");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_pictures, container, false);
+        ImageView imageView = rootView.findViewById(R.id.mainImageView);
+        imageView.setImageResource(photo);
+        return rootView;
+    }
+
+}
